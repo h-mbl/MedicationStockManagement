a74cb85e9fd73896d654cbed2352276d0cba613c
FaraDuMatin <118701688+FaraDuMatin@users.noreply.github.com>
2023-07-10 18:31:52 -0400

    precription probleme de contains

--- src/Tp2.java
+++ src/Tp2.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class Tp2 {
 
-    private static TreeMap<String,Medicament> arbreCommande = new TreeMap<>();
+    private static TreeMap<String, Medicament> arbreCommande = new TreeMap<>();
     private static TreeSet<Medicament> stock = new TreeSet<>();
-    public static  Date dateCourante = new Date(2000,01,01);
+    public static Date dateCourante = new Date(2000, 01, 01);
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Fournissez les noms des fichiers d'entrée et de sortie.");
@@ -16,17 +17,16 @@ public class Tp2 {
         String outputFile = "testCode/" + args[1];
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
             writer.write("");
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         try (FileReader fileReader = new FileReader(inputFile);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)){
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String line;
             int compteurPrescription = 0;
             while ((line = bufferedReader.readLine()) != null) {
                 String commande = line.split(" ")[0];
-                switch (commande){
+                switch (commande) {
                     case "DATE":
                         String dateLine = readDate(line);
                         String[] date = dateLine.split(" ");
@@ -43,19 +43,18 @@ public class Tp2 {
                                 String nomMedicament = prescription.getNomMedicament();
                                 int doseTraitement = prescription.getDoseTraitement();
                                 int repetition = prescription.getRepetition();
-                                prescription(nomMedicament, doseTraitement, repetition, stock, arbreCommande, writer);
+                                prescription(nomMedicament, doseTraitement, repetition, stock, arbreCommande, writer, compteurPrescription);
                             }
                             writer.write("\n");
-                        }
-                        catch (IOException e) {
+                        } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
 
                         break;
                     case "APPROV":
                         List<Medicament> approvListr = new ArrayList<>();
-                        approvListr = readApprov(bufferedReader, line) ;
-                        for (Medicament medicament:approvListr) {
+                        approvListr = readApprov(bufferedReader, line);
+                        for (Medicament medicament : approvListr) {
                             stock.add(medicament);
                         }
                         try (BufferedWriter writerApprov = new BufferedWriter(new FileWriter(outputFile, true))) {
@@ -70,20 +69,20 @@ public class Tp2 {
                         try (BufferedWriter writerStock = new BufferedWriter(new FileWriter(outputFile, true))) {
                             //String.format("%02d", number) spécifie que le nombre doit être formaté comme un nombre (%d)
                             //  de 2 caractères (2), et qu'un zéro sera ajouté devant le nombre si le nombre <10.
-                            writerStock.write("STOCK" +" "+dateCourante.getYear()+"-"+String.format("%02d", dateCourante.getMonth())+"-"+
+                            writerStock.write("STOCK" + " " + dateCourante.getYear() + "-" + String.format("%02d", dateCourante.getMonth()) + "-" +
                                     String.format("%02d", dateCourante.getDay()));
                             writerStock.newLine();
                             Iterator<Medicament> iterator = stock.iterator();
                             while (iterator.hasNext()) {
                                 Medicament medicament = iterator.next();
-                                if (medicament.getDateExpiration().getYear()< dateCourante.getYear() &&
-                                        medicament.getDateExpiration().getMonth()< dateCourante.getMonth() &&
-                                        medicament.getDateExpiration().getDay()< dateCourante.getDay() ) {
+                                if (medicament.getDateExpiration().getYear() < dateCourante.getYear() &&
+                                        medicament.getDateExpiration().getMonth() < dateCourante.getMonth() &&
+                                        medicament.getDateExpiration().getDay() < dateCourante.getDay()) {
                                     iterator.remove();
                                 } else {
                                     writerStock.write(medicament.getNom() + " " + medicament.getQuantite() + " " +
-                                            medicament.getDateExpiration().getYear() + "-" + String.format("%02d",medicament.getDateExpiration().getMonth()) + "-" +
-                                            String.format("%02d",medicament.getDateExpiration().getDay()));
+                                            medicament.getDateExpiration().getYear() + "-" + String.format("%02d", medicament.getDateExpiration().getMonth()) + "-" +
+                                            String.format("%02d", medicament.getDateExpiration().getDay()));
                                     writerStock.newLine();
                                 }
                             }
@@ -102,7 +101,7 @@ public class Tp2 {
         }
     }
 
-    public static String readDate (String line) {
+    public static String readDate(String line) {
         line = line.replace("-", " ");
         line = line.replace("DATE", "");
         line = line.replace(";", "");
@@ -111,7 +110,7 @@ public class Tp2 {
         return line;
     }
 
-    public static List<Prescription> readPrescription (BufferedReader bufferedReader, String line) throws IOException {
+    public static List<Prescription> readPrescription(BufferedReader bufferedReader, String line) throws IOException {
         List<Prescription> prescriptionsList = new ArrayList<>();
         while (!(line = bufferedReader.readLine()).equals(";")) {
             String[] prescriptionLine = line.split("\\s+");
@@ -120,16 +119,18 @@ public class Tp2 {
 
         return prescriptionsList;
     }
-    public static List<Medicament> readApprov (BufferedReader bufferedReader, String line) throws IOException {
+
+    public static List<Medicament> readApprov(BufferedReader bufferedReader, String line) throws IOException {
         List<Medicament> approvList = new ArrayList<>();
         while (!(line = bufferedReader.readLine()).equals(";")) {
             line = line.replace("-", " ");
             String[] approvLine = line.split("\\s+");
-            Date dateExpiration= new Date( Integer.parseInt(approvLine[2]), Integer.parseInt(approvLine[3]),Integer.parseInt(approvLine[4]));
-            approvList.add(new Medicament(approvLine[0], Integer.parseInt(approvLine[1]),dateExpiration));
+            Date dateExpiration = new Date(Integer.parseInt(approvLine[2]), Integer.parseInt(approvLine[3]), Integer.parseInt(approvLine[4]));
+            approvList.add(new Medicament(approvLine[0], Integer.parseInt(approvLine[1]), dateExpiration));
         }
         return approvList;
     }
+
     public static boolean datePasse(Date dateAVerifie, Date dateCourante) {
         if (dateAVerifie.getYear() < dateCourante.getYear()) {
             return true;
@@ -141,40 +142,47 @@ public class Tp2 {
             return false;
         }
     }
-    public static Date calculerJour(int days, int day, int month, int year){
+
+    public static Date calculerJour(int days, int day, int month, int year) {
         int newDay = day + days;
-        while(newDay > nbrJrMm(month,year)){
+        while (newDay > nbrJrMm(month, year)) {
             newDay -= month;
-            if (month == 12){month = 1; year += 1;}
-            else {month +=1;}
+            if (month == 12) {
+                month = 1;
+                year += 1;
+            } else {
+                month += 1;
+            }
         }
-        return new Date(newDay,month,year);
-    }
-    public static int nbrJrMm(int month, int year){
-        int[] nbrJr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if (month == 2 && isBissextile(year)){
-            return 29;
-        }
-        else {
-            return nbrJr[month-1];
-        }
-    }
-    public static boolean isBissextile(int year){
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-            return true;
-        }
-        else {return false;}
-    }
-    public static String writeDate(Date date){
-        return (date.getYear() + "-" +  String.format("%02d", date.getMonth()) + "-" + String.format("%02d", date.getDay()));
+        return new Date(newDay, month, year);
     }
 
-    public static Date date(Date date, TreeMap<String,Medicament> commande, String file){
+    public static int nbrJrMm(int month, int year) {
+        int[] nbrJr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2 && isBissextile(year)) {
+            return 29;
+        } else {
+            return nbrJr[month - 1];
+        }
+    }
+
+    public static boolean isBissextile(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String writeDate(Date date) {
+        return (date.getYear() + "-" + String.format("%02d", date.getMonth()) + "-" + String.format("%02d", date.getDay()));
+    }
+
+    public static Date date(Date date, TreeMap<String, Medicament> commande, String file) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-            if (commande.isEmpty()){
+            if (commande.isEmpty()) {
                 writer.write(writeDate(date) + " OK\n");
-            }
-            else {
+            } else {
                 writer.write(writeDate(date) + " COMMANDES :\n");
                 for (String nomMedicament : commande.keySet()) {
                     Medicament medicament = commande.get(nomMedicament);
@@ -188,80 +196,81 @@ public class Tp2 {
         }
         return date;
     }
-    public static void prescription(String nomMedicament, int doseTraitement, int repetition, TreeSet<Medicament> stock, TreeMap<String,Medicament> commande, BufferedWriter writer) throws IOException {
-        Medicament medicamentPrescris = new Medicament(nomMedicament, (doseTraitement*repetition));
-        //System.out.println(stock.size());
-        //System.out.println(medicamentPrescris.getNom());
-        if (stock.isEmpty()){
-            writer.write(nomMedicament + " " + doseTraitement + " " + repetition + " COMMANDE\n");
+
+    public static void prescription(String nomMedicament, int doseTraitement, int repetition, TreeSet<Medicament> stock, TreeMap<String, Medicament> commande, BufferedWriter writer, int compteur) throws IOException {
+        Medicament medicamentPrescris = new Medicament(nomMedicament, (doseTraitement * repetition));
+        if (stock.isEmpty()) {
+            writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  COMMANDE\n");
             String key = medicamentPrescris.getNom();
-            if (commande.containsKey(key)){
+            if (commande.containsKey(key)) {
                 // Changement du nombre de medicament donné à commander
                 Medicament medicamentCommande = commande.get(key);
                 medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
                 commande.put(key, medicamentCommande);
-            }
-            else {
+            } else {
                 commande.put(key, medicamentPrescris);
             }
         }
-        for (Medicament medicament : stock) {
-            //System.out.println(medicament.getNom());
-            if (medicament.getNom().equals(nomMedicament)) {
-                medicamentPrescris = medicament;
-                //System.out.println(medicamentPrescris.getNom());
-                //System.out.println("date courante: " + dateCourante);
-                //System.out.println("date expiration: " + medicamentPrescris.getDateExpiration());
-                if (!datePasse(medicamentPrescris.getDateExpiration(), dateCourante)){
+        if (stock.contains(medicamentPrescris)) {
+            for (Medicament medicament : stock) {
+                if (medicament.getNom().equals(nomMedicament)) {
+                    medicamentPrescris = medicament;
+                    if (!datePasse(medicamentPrescris.getDateExpiration(), dateCourante)) {
 
-                    if (medicamentPrescris.getQuantite() >= doseTraitement * repetition){
-                        writer.write(nomMedicament + " " + doseTraitement + " " + repetition + " OK\n");
-                        medicament.setQuantite(medicament.getQuantite() - doseTraitement*repetition);
-                        break;
-                    }
-                    else {
-                        writer.write(nomMedicament + " " + doseTraitement + " " + repetition + " COMMANDE\n");
+                        if (medicamentPrescris.getQuantite() >= doseTraitement * repetition) {
+                            writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  OK\n");
+                            medicament.setQuantite(medicament.getQuantite() - doseTraitement * repetition);
+                            break;
+                        } else {
+                            writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  COMMANDE\n");
+                            String key = medicamentPrescris.getNom();
+                            if (commande.containsKey(key)) {
+                                // Changement du nombre de medicament donné à commander
+                                Medicament medicamentCommande = commande.get(key);
+                                medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
+                                commande.put(key, medicamentCommande);
+                            } else {
+                                commande.put(key, medicamentPrescris);
+                            }
+                            break;
+                        }
+                    } else {
+                        writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  COMMANDE\n");
                         String key = medicamentPrescris.getNom();
-                        if (commande.containsKey(key)){
+                        if (commande.containsKey(key)) {
                             // Changement du nombre de medicament donné à commander
                             Medicament medicamentCommande = commande.get(key);
                             medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
                             commande.put(key, medicamentCommande);
-                        }
-                        else {
+                        } else {
                             commande.put(key, medicamentPrescris);
                         }
                         break;
                     }
-                }
-                else {
-                    writer.write(nomMedicament + " " + doseTraitement + " " + repetition + " COMMANDE\n");
+                } else {
+                    writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  COMMANDE\n");
                     String key = medicamentPrescris.getNom();
-                    if (commande.containsKey(key)){
-                        // Changement du nombre de medicament donné à commander
+                    if (commande.containsKey(key)) {
+                        // Changement du nombre de medicament donné à commandé
                         Medicament medicamentCommande = commande.get(key);
                         medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
                         commande.put(key, medicamentCommande);
-                    }
-                    else {
+                    } else {
                         commande.put(key, medicamentPrescris);
                     }
                     break;
                 }
             }
-            else {
-                writer.write(nomMedicament + " " + doseTraitement + " " + repetition + " COMMANDE\n");
-                String key = medicamentPrescris.getNom();
-                if (commande.containsKey(key)){
-                    // Changement du nombre de medicament donné à commandé
-                    Medicament medicamentCommande = commande.get(key);
-                    medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
-                    commande.put(key, medicamentCommande);
-                }
-                else {
-                    commande.put(key, medicamentPrescris);
-                }
-                break;
+        } else {
+            writer.write(nomMedicament + " " + doseTraitement + " " + repetition + "  COMMANDE\n");
+            String key = medicamentPrescris.getNom();
+            if (commande.containsKey(key)) {
+                // Changement du nombre de medicament donné à commandé
+                Medicament medicamentCommande = commande.get(key);
+                medicamentCommande.setQuantiteCommande(medicamentCommande.getQuantiteCommande() + medicamentPrescris.getQuantiteCommande());
+                commande.put(key, medicamentCommande);
+            } else {
+                commande.put(key, medicamentPrescris);
             }
         }
     }
